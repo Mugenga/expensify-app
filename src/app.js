474de75e6d18d1ca29b755fb1b7580165011ef7b
mugenga <rockyyves@gmail.com>
2020-05-20 19:42:34 +0200

    Set up app routes

--- src/app.js
+++ src/app.js
@@ -1,43 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import AppRouter from "./routers/AppRouter"
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-    <p>Expense</p>
-    </div>
-);
-const AddExpensePage = () => (
-    <div>
-    <p>create</p>
-    </div>
-);
-const EditExpensePage = () => (
-    <div>
-    <p>Edit</p>
-    </div>
-);
-const HelpPage = () => (
-    <div>
-    <p>Help</p>
-    </div>
-);
-const NotFoundPage = () => (
-    <div>
-    <p>404!</p>
-    </div>
-);
-const routes = (
-    <BrowserRouter>
-    <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-    </Switch>
-    </BrowserRouter>
-);
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
